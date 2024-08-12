e78c20f7e2d70a8d463f5b93987ee18946d6a1b0
SpikeSpieg-el <112814519+SpikeSpieg-el@users.noreply.github.com>
2024-08-13 02:41:50 +0500

    новая

--- server.py
+++ server.py
@@ -1,44 +1,68 @@
-from flask import Flask, jsonify, send_from_directory, render_template, abort
-from flask_cors import CORS
+from flask import Flask, render_template, jsonify, send_from_directory, request
 import os
 from datetime import datetime
 
 app = Flask(__name__)
-CORS(app)  # Разрешить CORS для всех доменов
 
-DIRECTORY = r'C:\Users\chash\Pictures\sd webui\webui\outputs\txt2img-images'
+# Укажите путь к папке, где хранятся файлы
+FILE_DIRECTORY = r'C:\Users\chash\Pictures\sd webui\webui\outputs\txt2img-images'
 
-def get_files_info(directory):
-    files = []
-    for root, dirs, filenames in os.walk(directory):
-        for filename in filenames:
-            filepath = os.path.join(root, filename)
-            stat = os.stat(filepath)
-            relative_path = os.path.relpath(filepath, directory)
-            files.append({
-                'name': filename,
-                'path': relative_path,
-                'mtime': stat.st_mtime,
-                'size': stat.st_size
-            })
-    files.sort(key=lambda x: x['mtime'], reverse=True)
-    return files
+def get_files_and_dirs(directory, sort_by=None, sort_order='asc', search_query=None):
+    entries = []
+    with os.scandir(directory) as it:
+        dirs = []
+        files = []
+        for entry in it:
+            if search_query and search_query.lower() not in entry.name.lower():
+                continue
+            if entry.is_dir():
+                dirs.append({
+                    'type': 'directory',
+                    'name': entry.name,
+                    'path': os.path.relpath(entry.path, FILE_DIRECTORY),
+                    'children': get_files_and_dirs(entry.path, sort_by, sort_order, search_query)
+                })
+            elif entry.is_file():
+                files.append({
+                    'type': 'file',
+                    'name': entry.name,
+                    'path': os.path.relpath(entry.path, FILE_DIRECTORY),
+                    'size': entry.stat().st_size,
+                    'mtime': datetime.fromtimestamp(entry.stat().st_mtime).strftime('%Y-%m-%d %H:%M:%S'),
+                    'ctime': datetime.fromtimestamp(entry.stat().st_ctime).strftime('%Y-%m-%d %H:%M:%S')
+                })
+        
+        # Сортировка директорий и файлов
+        def sort_key(entry):
+            if sort_by == 'size':
+                return entry.get('size', 0)
+            elif sort_by == 'date_created':
+                return entry.get('ctime', '')
+            elif sort_by == 'date_modified':
+                return entry.get('mtime', '')
+            else:  # default to name sorting
+                return entry.get('name', '').lower()
 
-@app.route('/', methods=['GET'])
-def home():
+        if sort_by:
+            dirs.sort(key=sort_key, reverse=(sort_order == 'desc'))
+            files.sort(key=sort_key, reverse=(sort_order == 'desc'))
+
+        return dirs + files
+
+@app.route('/')
+def index():
     return render_template('index.html')
 
-@app.route('/files', methods=['GET'])
-def list_files():
-    files_info = get_files_info(DIRECTORY)
-    return jsonify(files_info)
+@app.route('/api/contents')
+def contents():
+    sort_by = request.args.get('sort_by')
+    sort_order = request.args.get('sort_order', 'asc')  # Default to ascending order
+    search_query = request.args.get('search_query')
+    return jsonify(get_files_and_dirs(FILE_DIRECTORY, sort_by, sort_order, search_query))
 
-@app.route('/files/<path:filename>', methods=['GET'])
-def serve_file(filename):
-    filepath = os.path.join(DIRECTORY, filename)
-    if not os.path.isfile(filepath):
-        abort(404)  # Файл не найден
-    return send_from_directory(DIRECTORY, filename)
+@app.route('/files/<path:filename>')
+def get_file(filename):
+    return send_from_directory(FILE_DIRECTORY, filename)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000, debug=True)
